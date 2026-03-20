3303471d7f54938fe7af0c1dbc07a5f13842064d
Israel García <42307512+garcii06@users.noreply.github.com>
2026-03-20 15:17:21 -0600

    Update streamlit_app.py
    Fixed choose typo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
-    'Shoose up to 5 ingredients:'
+    'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections = 5
 )
